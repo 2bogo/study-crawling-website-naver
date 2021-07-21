f16727b591b98c8ae88083f4fda15ee8102f95ba
JinhaeCoder <justezy0210@gmail.com>
2021-07-21 19:27:38 +0900

    daumCrawling

--- daum/daumCrawlingLinks.py
+++ daum/daumCrawlingLinks.py
@@ -8,6 +8,7 @@ import pickle
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+from tqdm import tqdm
 
 def date_range(start, end):
     start = datetime.strptime(start, "%Y-%m-%d")
@@ -21,29 +22,21 @@ def searchTotalLink(date):
     page = 1
     joinedDate = ''.join(date.split('.'))
     while True:
-        addr = f"https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EA%B2%BD%EB%82%A8&sort=0&photo=0&field=0&pd=3&ds={date}&de={date}&cluster_rank=21&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:from{joinedDate}to{joinedDate},a:all&start={page}"
+        addr = f"https://search.daum.net/search?w=news&DA=STC&enc=utf8&cluster=y&cluster_page=1&q=%EA%B2%BD%EB%82%A8&period=u&sd={date}000000&ed={date}235959&p={page}"
         driver.get(addr)
         time.sleep(1)
         html = driver.page_source
         dom = BeautifulSoup(html, "lxml")
-        try:
-            if "검색결과가 없습니다." in [x.text for x in dom.find_all("div", {"class" : "not_found02"})][0]:
-                break
-        except:
-            select_raw = dom.find_all("a", {"class" : "info"})
-            links = [selected.attrs['href'] for selected in select_raw]
+        select_raw = dom.find_all("a", {"class" : "f_nb"})
+        links = [selected.attrs['href'] for selected in select_raw]
 
-            if lastLink == links[-1]:
-                break
-            else:
-                lastLink = links[-1]
-
-            naver_links = [x.split('mode=LSD')[0] + "m_view=1&includeAllCount=true&mode=LSD" + x.split('mode=LSD')[1] for x in links if 'naver' in x and 'mode=LSD' in x]
-            totalLink += naver_links
-            page += 10
+        if lastLink == links[-1]:
+            break
+        else:
+            lastLink = links[-1]
+            totalLink += links
+            page += 1
     return totalLink
-
-
 
 def crawlingComments(addr):
     driver.get(addr)
@@ -52,7 +45,7 @@ def crawlingComments(addr):
     
     try:
         while True:
-            driver.find_element_by_css_selector(".u_cbox_btn_more").click()
+            driver.find_element_by_css_selector(".link_fold").click()
             time.sleep(1.5)
             pages+=1
             
@@ -65,7 +58,7 @@ def crawlingComments(addr):
     html = driver.page_source
     dom = BeautifulSoup(html, "lxml")
     
-    title_raw = dom.find_all("h3", {"id" : "articleTitle"})
+    title_raw = dom.find_all("h3", {"class" : "tit_view"})
     
     try:
         title = [x.text for x in title_raw][0]
@@ -73,13 +66,13 @@ def crawlingComments(addr):
         title = title_raw
     
     
-    comments_raw = dom.find_all("span", {"class" : "u_cbox_contents"})
+    comments_raw = dom.find_all("p", {"class" : "desc_txt"})
     if comments_raw == []:
         pass
     else:
         comments = [[title, comment.text, addr] for comment in comments_raw]
         return comments
-
+    
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
 options.add_argument("--no-sandbox")
@@ -87,9 +80,9 @@ options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome('/home/ezy/Downloads/chromedriver', options=options)
 
-dates = date_range("2020-7-16", "2021-07-17")
+dates = date_range("2021-7-21", "2021-07-21")
 
-totalLinks = [searchTotalLink(x) for x in dates]
+totalLinks = [searchTotalLink(x) for x in tqdm(dates)]
 
 totalLinks = sum(totalLinks, [])
 
@@ -97,8 +90,9 @@ import pickle
 
 with open('totalLinks.pkl', 'wb') as f:
     pickle.dump(totalLinks, f)
-
-data = [crawlingComments(addr) for addr in totalLinks]
+    
+    
+data = [crawlingComments(addr) for addr in tqdm(totalLinks)]
 
 data = list(filter(None, data))
 
@@ -107,6 +101,6 @@ data = sum(data, [])
 data = np.stack(data)
 df = pd.DataFrame(data, columns=['title', 'comment', 'link'])
 
-df.to_pickle('naverComment.pk')
+df.to_pickle('daumComment.pk')
 
 driver.close()
